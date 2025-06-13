85f461092d973270dd48307b25b77361d82980bd
Meriem11111 <abdlk.meriem00@gmail.com>
2025-06-13 22:19:29 +0100

    ex00 done

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -43,10 +43,33 @@ bool CheckFloat(const std::string &str)
     return true;
 }
 
-// bool checkInt(const std::string &str)
-// {
-//     std::strtoint
-// }
+
+bool CheckDouble(const std::string &str)
+{
+    // if(str[str.length() -1 ] != 'f')
+    //     return false;
+
+    size_t i = 0;
+    int count = 0;
+
+    if(str[i] == '+' || str[i] == '-')
+        i++;
+    
+    while(i < str.length() - 1)
+    {
+        if(str[i] == '.' && count == 0)
+        {
+            i++;
+            count++;
+        }
+        if(!std::isdigit(str[i]))
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
 
 bool checkDigit(const std::string &str)
 {
@@ -71,8 +94,11 @@ void ScalarConverter::convert(const std::string &literal)
         char c = literal[0];
         std::cout << "char: " << c <<std::endl;
         std::cout << "int: " << static_cast<int>(c)  <<std::endl;
-        std::cout << "float: " << static_cast<float>(c)  <<std::endl;
+        // std::cout << "float: " << static_cast<float>(c)  <<std::endl;
+        std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(c)  << 'f' <<std::endl;
         std::cout << "double: " << static_cast<double>(c)  <<std::endl;
+        // std::cout << "double: " <<  std::fixed << std::setprecision(2) <<static_cast<double>(c)  <<std::endl;
+
 
     }
     else if(checkDigit(literal))
@@ -87,11 +113,11 @@ void ScalarConverter::convert(const std::string &literal)
         }
         std::cout << "int: " << static_cast<int>(nbr) <<std::endl;
 
-        if(std::isprint(static_cast<char>(nbr)) && static_cast<int>(nbr) > 32 && static_cast<int>(nbr) < 127) // check >= 32 if i add it ??
+        if(std::isprint(static_cast<char>(nbr)) ) // check >= 32 if i add it ??
             std::cout << "char: " << static_cast<char>(nbr) <<std::endl;
         else
             std::cout << "char: Non displayable" << std::endl;
-        std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(nbr)  <<std::endl;
+        std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(nbr)  << 'f' <<std::endl;
         std::cout << "double: " <<  std::fixed << std::setprecision(2) <<static_cast<double>(nbr)  <<std::endl;
 
     }
@@ -105,10 +131,41 @@ void ScalarConverter::convert(const std::string &literal)
             std::cout << "double: " << literal.substr(0, literal.length() - 1)  <<std::endl;
             return;
         }
-        else 
-        {
-            
-        }
+        std::istringstream num(literal);
+        double nbr ;
+        num >> nbr;
+        
+        std::cout << "int: " << static_cast<int>(nbr) <<std::endl;
+
+        if(std::isprint(static_cast<char>(nbr)) ) // check >= 32 if i add it ??
+            std::cout << "char: " << static_cast<char>(nbr) <<std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
+        std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(nbr)  << "f" <<std::endl;
+        std::cout << "double: " <<  std::fixed << std::setprecision(2) <<static_cast<double>(nbr)  <<std::endl;
     }
-    std::cout << "IN" << std::endl;
+     else if(CheckDouble(literal) || literal == "-inf" || literal == "+inf" || literal == "nan")
+     {
+         if(literal == "-inf" || literal == "+inf" || literal == "nan")
+        {
+            std::cout << "char: " << "impossible" <<std::endl;
+            std::cout << "int: " << "impossible"  <<std::endl;
+            std::cout << "float: " << literal <<'f'  <<std::endl;
+            std::cout << "double: " << literal <<std::endl;
+            return;
+        }
+        std::istringstream num(literal);
+        double nbr ;
+        num >> nbr;
+        
+        std::cout << "int: " << static_cast<int>(nbr) <<std::endl;
+
+        if(std::isprint(static_cast<char>(nbr)) ) // check >= 32 if i add it ??
+            std::cout << "char: " << static_cast<char>(nbr) <<std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
+        std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(nbr)  << 'f' <<std::endl;
+        std::cout << "double: " <<  std::fixed << std::setprecision(2) <<static_cast<double>(nbr)  <<std::endl;
+    
+     }
 }
