061aaafa4f646de2d309710fb6691c17ffa72468
Meriem11111 <abdlk.meriem00@gmail.com>
2025-07-06 18:59:13 +0100

    cpp8 done

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,43 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <list>
+#include <exception>
+#include <algorithm>
+#include <climits>
+#include<ctime>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() {}
+        MutantStack(const MutantStack& org)
+        {
+            *this = org;
+        }
+        MutantStack& operator=(const MutantStack& org)
+        {
+            if(this != &org)
+                *this = org;
+            return(*this);
+        }
+        ~MutantStack() {}
+        
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin()
+        {
+            return(this->c.begin());
+        }
+        iterator end()
+        {
+            return(this->c.end());
+        }
+
+
+};
+
+#endif
